2cae9263f0dbb4b6e70b65e2bfd2850eb91f0243
punithsabitha245 <punith.sabitha@gmail.com>
2026-09-19 17:14:20 +0530

    Add files via upload

---
+++ static.java
@@ -0,0 +1,20 @@
+class Counter{
+    int number=10;
+
+    void increment{}{
+        number=number+1
+    }
+
+public static void main(String[] args){
+    Counter obj1=new Counter();
+    Counter obj2=new counter();
+    Counter obj3=new counter();
+
+
+obj1.increment();
+obj2.increment();
+obj3.increment();
+
+System.out.println(obj1,number);
+}
+}
